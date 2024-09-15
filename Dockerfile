b3d1f7f82a2cc6c6de34269580ccef4097f873bc
PaddeCraft <info@paddecraft.dev>
2024-09-15 20:15:26 +0200

    fix: Fix entrypoint script erroring because breaking changes in a dependency

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ FROM python:3.12.0-alpine
 VOLUME [ "/config" ]
 
 RUN mkdir /app && \
-    pip install porkbun-ddns apscheduler
+    pip install porkbun-ddns==1.1.2 apscheduler==3.10.4
 
 WORKDIR /app
 COPY ./entrypoint.py /app
--- entrypoint.py
+++ entrypoint.py
@@ -1,4 +1,6 @@
 from porkbun_ddns import PorkbunDDNS
+from porkbun_ddns.config import Config, DEFAULT_ENDPOINT
+
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 import os
@@ -6,11 +8,7 @@ import json
 
 sched = BlockingScheduler()
 
-DEFAULT_CONFIG = {
-    "key": "YOUR_API_KEY",
-    "secret": "YOUR_API_SECRET",
-    "domains": []
-}
+DEFAULT_CONFIG = {"key": "YOUR_API_KEY", "secret": "YOUR_API_SECRET", "domains": []}
 
 CONFIG_PATH = "/config/config.json"
 
@@ -27,21 +25,22 @@ pb_objects = []
 
 for domain in config["domains"]:
     parts = domain.rsplit(".", 2)
-    
-    pb = PorkbunDDNS({
-        "secretapikey": config["secret"],
-        "apikey": config["key"]
-    }, ".".join(parts[1:]))
+
+    pb = PorkbunDDNS(
+        Config(DEFAULT_ENDPOINT, config["key"], config["secret"]), ".".join(parts[1:])
+    )
     pb.set_subdomain(parts[0])
-    
+
     pb_objects.append(pb)
 
-@sched.scheduled_job('interval', hours=1)
+
+@sched.scheduled_job("interval", hours=1)
 def update():
     print("Updating...")
     for pb in pb_objects:
         pb.update_records()
     print("Updated.")
+
 
 update()
 sched.start()
